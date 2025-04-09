afac38a2fde5a8559f8e9517ba4d589d11c6b04a
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-09 08:48:37 -0400

    main added

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,11 @@
 const TABLE_SELECTOR = ".table.target-audience-table";
 
+
 function scrapeAndExport(exportType) {
   return exportType; // each btn can determine context
+}
+
+// main logic - check reference
+function main() {
+  return 0;
 }
