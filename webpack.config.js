9d5bba510199709c223431516ec4db9bc09e4133
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-09 10:24:03 -0400

    pro and dev and stats added, with main pops

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'inline-source-map',
+
+    entry: {
+      popup: './src/popup/popup.js',
+      content_script: './src/content/content_script.js',
+    },
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'popup.bundle.js',
+      clean: true,
+    },
+
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: 'src/manifest.json', to: 'manifest.json' },
+          { from: 'src/icons', to: 'icons' },
+          { from: 'src/popup/popup.html', to: 'popup/popup.html' },
+          // Let's first see if needed:
+          // { from: './tailwindcss_cdn_4.1.3.js', to: './tailwindcss_cdn_4.1.3.js' }
+        ],
+      }),
+    ],
+
+    resolve: {
+      extensions: ['.js'],
+    },
+  };
+};
