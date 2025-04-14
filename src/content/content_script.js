708b1bdef50ed5ab86802f389dd3edec2ca37556
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-14 00:37:38 -0400

    del and added ping response

--- src/content/content_script.js
+++ src/content/content_script.js
@@ -6,8 +6,7 @@ if (!hasListener) {
 
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "PING") {
-      sendResponse({ status: "PONG" });
-      // indication of will to sendResponse async.
+      sendResponse({ status: "PING" });
       return true;
     }
 
@@ -24,10 +23,5 @@ if (!hasListener) {
     }
     return undefined; // I say it's fine, since it speaks for itself.
   });
-
-  console.log("Listening...");
 } else {
-  console.log("Already listening. (30)");
 }
-
-console.log("Done: Process has been executed.");
