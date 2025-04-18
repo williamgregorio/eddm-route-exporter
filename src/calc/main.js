c413619487946148e5564a7d56853ae2fa698207
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-18 14:11:28 -0400

    initial idea, input data, parse, tranform, set goal on route goal to pick the best

--- src/calc/main.js
+++ src/calc/main.js
@@ -1,1 +1,35 @@
-# will run some test with data
+let rawData = ``
+
+let lines = rawData.trim().split("\n")
+let headerLines = lines.shift()
+let dataLines = lines;
+
+
+let headers = headerLines.split(",")
+
+let processedData = dataLines.map(line => {
+    let values = line.split(',')
+    let rowObject = {}
+
+    headers.forEach((header,index) => {
+        let stringValue = values[index]
+        let processedValue;
+        if (header === "Residential" || header === "Business" || header === "Total") {
+            processedValue = parseInt(stringValue, 10)
+        } else if (header === "Age: 25-34") {
+            processedValue = parseFloat(stringValue.replace("%", "")) / 100
+        } else if (header === "Size" || header === "Cost") {
+            processedValue = parseFloat(stringValue.replace("$", ""))
+        } else if (header === "Income") {
+            processedValue = parseFloat(stringValue.replace("$", "").replace("k", "")) * 1000
+        } else {
+            processedValue = stringValue
+        }
+        rowObject[header] = processedValue
+    })
+
+    return rowObject;
+})
+
+console.log(processedData)
+
